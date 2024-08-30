11e2515912e00b484db45467d77a78e7b6c7a829
Demi-la <adebiyio28@gmail.com>
2024-08-30 15:12:39 +0100

    finished features section

--- src/pages/enterprise/data.jsx
+++ src/pages/enterprise/data.jsx
@@ -1,6 +1,9 @@
 import dial from "../../../public/assets/landingPageAssets/dial.png";
 import track from "../../../public/assets/landingPageAssets/track.png";
 import chart from "../../../public/assets/landingPageAssets/chart.png";
+import analytics from "../../../public/assets/landingPageAssets/analytics.png";
+import Optimization from "../../../public/assets/landingPageAssets/Optimization.png";
+import dispatch from "../../../public/assets/landingPageAssets/dispatch.png";
 export const howItWorks = [
   {
     icon: track,
@@ -19,5 +22,29 @@ export const howItWorks = [
     title: "Control sources in real time",
     description:
       "Ensure uninterrupted operations by seamlessly switching between various energy sources ",
+  },
+];
+export const features = [
+  {
+    image: analytics,
+    title: "Energy Analytics",
+    subtitle: "Track energy usage and availability patterns ",
+    description:
+      "Understand your historical power supply patterns, see them change in real-time, and be able to proactively manage your energy consumption such as outages before they occur",
+  },
+  {
+    image: Optimization,
+    title: "Sizing Optimizations",
+    subtitle: "Accurately size future energy sources",
+    description:
+      "Determine the most optimal combination of power sources to reduce your total power costs to their lowest levels possible, based on your energy demand per time.",
+  },
+  {
+    image: dispatch,
+    title: "Automated Dispatch",
+    subtitle:
+      "Automatically switch between different energy sources in real-time",
+    description:
+      "Utilize prescriptive dispatch schedules to operate your various power sources in a concerted fashion to reduce total power costs to the lowest levels possible, based on your energy demand.",
   },
 ];
--- src/pages/enterprise/index.jsx
+++ src/pages/enterprise/index.jsx
@@ -2,12 +2,14 @@ import React from 'react'
 import Navigation from "../../components/navigation"
 import HeroSection from './HeroSection';
 import HowPaiWorks from './HowPaiWorks';
+import Features from './Features';
 
 export default function Enterprise  () {
   return <>
   <Navigation />
   <HeroSection/>
   <HowPaiWorks/>
+  <Features/>
   </>;
 }
 
