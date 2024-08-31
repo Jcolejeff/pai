6ddd54e2a883b304cad72ec70696a7936580a744
Jcolejeff <ikwuhjefferye@gmail.com>
2024-08-31 17:44:56 +0100

    chore: Update npm dependencies and styles for landing page

--- src/pages/landingPage/HeroSection.jsx
+++ src/pages/landingPage/HeroSection.jsx
@@ -1,42 +1,50 @@
 import { Link } from "react-router-dom";
 import { HiArrowSmallRight } from "react-icons/hi2";
+import { Fade, Slide } from "react-awesome-reveal";
 export default function HeroSection() {
 	return (
-    <>
-      <div className="w-full lg:min-h-screen pt-[7.5rem] px-4 flex flex-col items-center font-general-sans">
-        <p className="lg:text-center lg:text-[3.5rem] md:text-center md:text-[3.5rem]  font-semibold text-[2.813rem] lg:leading-[110%] leading-[120%] lg:max-w-[40%] md:max-w-[70%]">
-          Minimize Cost. Maximize Uptime.
-        </p>
-        <p className="lg:text-center md:text-center text-primary-2 font-light  text-[1.125rem] lg:text-[1.25rem] md:text-[1.25rem] lg:w-[60%] md:w-[90%] mt-2">
-          Pai helps you spend less on operating your power supply sources while
-          maintaining the maximum uptime needed to sustain and grow your
-          operations.
-          <br />
-          More energy at the lowest possible cost.
-        </p>
-        <div className="inter-font flex gap-4 mt-8 w-[100%]  lg:justify-center md:justify-center">
-          <Link to="/">
-            <button className="bg-custom-blue rounded-md font-medium text-white mx-auto px-8 py-3 text-[0.875rem]">
-              Book a demo
-            </button>
-          </Link>
-          <Link to="/">
-            <button className="bg-transparent border  border-secondary-6 rounded-md font-medium text-secondary-6 mx-auto px-4 py-[0.5rem] text-[0.875rem] flex items-center">
-              Watch webinar{" "}
-              <span>
-                <HiArrowSmallRight className="text-[1.5rem] ml-2 mt-1" />
-              </span>
-            </button>
-          </Link>
-        </div>
-        <div className="relative lg:bg-splash bg-no-repeat bg-[center_top_15rem] w-screen mt-10  justify-center bg-contain  flex">
-          <img
-            src="/assets/landingPageAssets/heroImage.png"
-            alt="Dashboard"
-            aria-hidden
-          />
-        </div>
-      </div>
-    </>
-  );
+		<>
+			<div className="w-full lg:min-h-screen pt-[7.5rem] px-4 flex flex-col items-center font-general-sans">
+				<Fade
+					direction="up"
+					triggerOnce
+					className="lg:max-w-[40%] md:max-w-[70%]"
+				>
+					<h1 className="lg:text-center lg:text-[3.5rem] md:text-center md:text-[3.5rem]  font-semibold text-[2.813rem] lg:leading-[110%] leading-[120%] ">
+						Minimize Cost. Maximize Uptime.
+					</h1>
+				</Fade>
+
+				<p className="lg:text-center md:text-center text-primary-2 font-light  text-[1.125rem] lg:text-[1.25rem] md:text-[1.25rem] lg:w-[60%] md:w-[90%] mt-2">
+					Pai helps you spend less on operating your power supply sources while
+					maintaining the maximum uptime needed to sustain and grow your
+					operations.
+					<br />
+					More energy at the lowest possible cost.
+				</p>
+				<div className="inter-font flex gap-4 mt-8 w-[100%]  lg:justify-center md:justify-center">
+					<Link to="/">
+						<button className="bg-custom-blue rounded-md font-medium text-white mx-auto px-8 py-3 text-[0.875rem]">
+							Book a demo
+						</button>
+					</Link>
+					<Link to="/">
+						<button className="bg-transparent border  border-secondary-6 rounded-md font-medium text-secondary-6 mx-auto px-4 py-[0.5rem] text-[0.875rem] flex items-center">
+							Watch webinar{" "}
+							<span>
+								<HiArrowSmallRight className="text-[1.5rem] ml-2 mt-1" />
+							</span>
+						</button>
+					</Link>
+				</div>
+				<div className="relative lg:bg-splash bg-no-repeat bg-[center_top_15rem] w-screen mt-10  justify-center bg-contain  flex">
+					<img
+						src="/assets/landingPageAssets/heroImage.png"
+						alt="Dashboard"
+						aria-hidden
+					/>
+				</div>
+			</div>
+		</>
+	);
 }
--- src/pages/landingPage/HowPaiWorks.jsx
+++ src/pages/landingPage/HowPaiWorks.jsx
@@ -1,45 +1,48 @@
+import { Fade } from "react-awesome-reveal";
 import { howItWorks } from "./data";
 export default function HowPaiWorks() {
-  return (
-    <>
-      <div className=" px-4 flex flex-col lg:items-center md:items-center  font-general-sans lg:mt-[10rem] mt-[5rem] ">
-        <p className="text-secondary-2 text-[1.125rem] font-semibold">
-          How Pai Works
-        </p>
-        <p className="lg:text-center md:text-center lg:text-[3rem] font-semibold text-[2.375rem] lg:leading-[130%] leading-[120%] lg:max-w-[60%] md:max-w-[80%]">
-          Intelligent energy management for your business
-        </p>
-        <p className="lg:text-center md:text-center text-primary-2 font-normal  text-[1rem] lg:text-[1.125rem] md:text-[1.125rem] lg:w-[60%] md:w-[90%] mt-2">
-          Pai leverages real-time pattern recognition from historical energy
-          data across your power supply sources, using AI, to predict costs and
-          reduce losses, accurately size future assets for cheaper procurement
-          costs and automatically switch between different
-          <span className="hidden lg:inline">
-            <br />
-          </span>{" "}
-          power sources for uninterrupted operations
-        </p>
-        <div className="grid grid-cols-1 md:grid-cols-3 gap-1 mt-[3rem]">
-          {howItWorks.map((item, index) => (
-            <div
-              key={index}
-              className="text-center p-4  shadow-md flex flex-col items-center"
-            >
-              <img
-                src={item.icon}
-                alt={item.title}
-                className="mx-auto mb-4 w-16 h-16"
-              />
-              <h3 className="text-[1.375rem] font-semibold mb-2">
-                {item.title}
-              </h3>
-              <p className="text-secondary-3 font-normal text-[1rem] w-[90%] text-center mt-2">
-                {item.description}
-              </p>
-            </div>
-          ))}
-        </div>
-      </div>
-    </>
-  );
+	return (
+		<>
+			<div className=" px-4 flex flex-col lg:items-center md:items-center  font-general-sans lg:mt-[10rem] mt-[5rem] ">
+				<p className="text-secondary-2 text-[1.125rem] font-semibold">
+					How Pai Works
+				</p>
+				<p className="lg:text-center md:text-center lg:text-[3rem] font-semibold text-[2.375rem] lg:leading-[130%] leading-[120%] lg:max-w-[60%] md:max-w-[80%]">
+					Intelligent energy management for your business
+				</p>
+				<p className="lg:text-center md:text-center text-primary-2 font-normal  text-[1rem] lg:text-[1.125rem] md:text-[1.125rem] lg:w-[60%] md:w-[90%] mt-2">
+					Pai leverages real-time pattern recognition from historical energy
+					data across your power supply sources, using AI, to predict costs and
+					reduce losses, accurately size future assets for cheaper procurement
+					costs and automatically switch between different
+					<span className="hidden lg:inline">
+						<br />
+					</span>{" "}
+					power sources for uninterrupted operations
+				</p>
+				<div className="grid grid-cols-1 md:grid-cols-3 gap-1 mt-[3rem]">
+					<Fade cascade triggerOnce>
+						{howItWorks.map((item, index) => (
+							<div
+								key={index}
+								className="text-center p-4  shadow-md flex flex-col items-center"
+							>
+								<img
+									src={item.icon}
+									alt={item.title}
+									className="mx-auto mb-4 w-16 h-16"
+								/>
+								<h3 className="text-[1.375rem] font-semibold mb-2">
+									{item.title}
+								</h3>
+								<p className="text-secondary-3 font-normal text-[1rem] w-[90%] text-center mt-2">
+									{item.description}
+								</p>
+							</div>
+						))}
+					</Fade>
+				</div>
+			</div>
+		</>
+	);
 }
